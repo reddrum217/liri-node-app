b6a90faa2016c6f6750e9a60d3ef70a33bec0692
Daniel Vreeland <reddrum217@gmail.com>
2017-01-28 00:25:06 -0500

    Added Packages

--- keys.js
+++ keys.js
@@ -1,8 +1,9 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'xMaYs1wjzYdHuvyqlGMmFW4wW',
+  consumer_secret: 'eaIQYXnHNL10wyxvAP1yFL9UPzrRAFsN7XZa8jwzApm8sCHqks',
+  access_token_key: '824124236382044160-go0oGyOONWxXXWy89qu78rovxkDwMVi',
+  access_token_secret: 'nrraqR5cyawiA2Fb4IGeID8zOpBizjm69oSoLvDwtbbb2',
 }
+
---
+++ liri.js
@@ -0,0 +1,31 @@
+var t = require('./keys.js');
+
+console.log(t);
+
+
+// First require Twitter
+var Twitter = require('twitter');
+
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+
+// Require Spotify API
+var spotify = require('spotify');
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // Do something with 'data' 
+});
+
+
+//Require OMDB
